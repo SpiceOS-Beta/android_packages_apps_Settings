6429f92fcf9cea3ae4f7132ebbe3e993c32e23df
LuK1337 <priv.luk@gmail.com>
2018-08-26 15:00:21 +0200

    Settings: Add SpiceOS entries into device info
    Author: Harry Youd <harry@harryyoud.co.uk>
    Date:   Fri Nov 24 09:49:51 2017 -0800
    
        Settings: Add LineageOS version and build date to device info
    
        Change-Id: I4acf834442712954f8e9db15370bffafd4547174
    
    Change-Id: I8d4c6869f8dfa34a96567bec18f9f8276b66a64e
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: Anush02198 <anush.4376@gmail.com>

---
+++ src/com/android/settings/deviceinfo/firmwareversion/SpiceOSVersionDetailPreferenceController.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.settings.core.BasePreferenceController;
+import com.android.settings.slices.Sliceable;
+
+public class SpiceOSVersionDetailPreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "SpiceOSVersionDialogCtrl";
+
+    private static final String KEY_SPICEOS_VERSION_PROP = "ro.spiceos.version";
+
+    public SpiceOSVersionDetailPreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public boolean useDynamicSliceSummary() {
+        return true;
+    }
+
+    @Override
+    public boolean isSliceable() {
+        return true;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return SystemProperties.get(KEY_SPICEOS_VERSION_PROP,
+                mContext.getString(R.string.unknown));
+    }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
+            return false;
+        }
+        if (Utils.isMonkeyRunning()) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void copy() {
+        Sliceable.setCopyContent(mContext, getSummary(),
+                mContext.getText(R.string.spiceos_version));
+    }
+}
