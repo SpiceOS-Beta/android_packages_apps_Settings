2fd7dde3581d6cb69ebd623f517327cfeb12a1d3
Pranav Vashi <neobuddy89@gmail.com>
2019-11-17 02:30:00 +0530

    Bring back Gaming mode [2/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iac1b92118a722c35891feac836667543aa0c3cdc
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -160,6 +160,7 @@ import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.spiceos.settings.SpiceOSSettings;
+import com.spiceos.settings.fragments.misc.GamingMode;
 
 public class SettingsGateway {
 
@@ -310,6 +311,7 @@ public class SettingsGateway {
             InteractAcrossProfilesDetails.class.getName(),
             MediaControlsSettings.class.getName(),
             SpiceOSSettings.class.getName()
+            GamingMode.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -350,5 +352,6 @@ public class SettingsGateway {
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
             Settings.SpiceOSSettingsActivity.class.getName(),
+            Settings.GamingModeActivity.class.getName(),
     };
 }
